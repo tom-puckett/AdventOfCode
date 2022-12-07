4a77c2b5f69767d0bff29a2f8a67f821c73e1a0e
tom.puckett <tom.puckett@milliman.com>
2022-12-06 22:29:01 -0500

    Using only top level code

--- AdventOfCode2022/Day6/Program.cs
+++ AdventOfCode2022/Day6/Program.cs
@@ -1,17 +1,8 @@
-﻿namespace Day6
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            string dataStream = File.ReadAllText("InputData.txt");
+﻿string dataStream = File.ReadAllText("InputData.txt");
 
-            foreach (int markerLength in new[] {4,14})
-            {
-                int markerPosition = markerLength;
-                for (; markerPosition <= dataStream.Length && dataStream.Substring(markerPosition-markerLength, markerLength).Distinct().Count() < markerLength; markerPosition++) { }
-                Console.WriteLine($"Marker postion 1 is {markerPosition}");
-            }
-        }
-    }
+foreach (int markerLength in new[] {4,14})
+{
+    int markerPosition = markerLength;
+    for (; markerPosition <= dataStream.Length && dataStream.Substring(markerPosition-markerLength, markerLength).Distinct().Count() < markerLength; markerPosition++) { }
+    Console.WriteLine($"Marker postion 1 is {markerPosition}");
 }
