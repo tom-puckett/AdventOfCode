0d788d22bbe185f387a8b5f78a9c3cc05882f60c
tom.puckett <tom.puckett@milliman.com>
2022-12-06 22:33:21 -0500

    Correct output text

--- AdventOfCode2022/Day6/Program.cs
+++ AdventOfCode2022/Day6/Program.cs
@@ -4,5 +4,5 @@ foreach (int markerLength in new[] {4,14})
 {
     int markerPosition = markerLength;
     for (; markerPosition <= dataStream.Length && dataStream.Substring(markerPosition-markerLength, markerLength).Distinct().Count() < markerLength; markerPosition++) { }
-    Console.WriteLine($"Marker postion 1 is {markerPosition}");
+    Console.WriteLine($"For marker length {markerLength}, marker postion is {markerPosition}");
 }
