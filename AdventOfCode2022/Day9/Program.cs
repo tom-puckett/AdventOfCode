fb995e75ab96549e22853508acce200675952921
tom.puckett <tom.puckett@milliman.com>
2022-12-09 01:43:28 -0500

    Minor cleanup, better names, etc.

--- AdventOfCode2022/Day9/Program.cs
+++ AdventOfCode2022/Day9/Program.cs
@@ -3,15 +3,13 @@
     internal class Program
     {
         static HashSet<(int, int)> part1PositionsVisited = new HashSet<(int, int)>(new PositionComparer()) { (0,0) };
-        static HashSet<(int, int)> part2PositionsVisited = new HashSet<(int, int)>(new PositionComparer()) { (0,0) };
-        //static (int row, int col) headPosition = (0, 0);
-        //static (int row, int col) tailPosition = (0, 0);
         static List<(int row, int col)> part1Knots = new List<(int row, int col)>
         {
             (0, 0),  // head
             (0, 0)   // tail
         };
 
+        static HashSet<(int, int)> part2PositionsVisited = new HashSet<(int, int)>(new PositionComparer()) { (0, 0) };
         static List<(int row, int col)> part2Knots = new List<(int row, int col)>
         {
             (0, 0),  // head 
@@ -31,9 +29,9 @@
             foreach (string line in File.ReadLines("InputData.txt"))
             {
                 var components = line.Split(' ');
-                (char direction, int number) instruction = (components[0][0], int.Parse(components[1]));
+                (char direction, int count) instruction = (components[0][0], int.Parse(components[1]));
                 Console.WriteLine($"Instruction is {instruction}");
-                for (int i=0; i<instruction.number; i++)
+                for (int i=0; i<instruction.count; i++)
                 {
                     switch (instruction.direction)
                     {
@@ -54,42 +52,43 @@
                             part2Knots[0] = (part2Knots[0].row, part2Knots[0].col - 1);
                             break;
                     }
+
                     for (int j=1; j< part1Knots.Count; j++)
                     {
-                        part1Knots[j] = MoveTail(part1Knots[j-1], part1Knots[j]);
+                        part1Knots[j] = MoveNextKnot(part1Knots[j-1], part1Knots[j]);
                     }
                     part1PositionsVisited.Add(part1Knots.Last());
+
                     for (int j = 1; j< part2Knots.Count; j++)
                     {
-                        part2Knots[j] = MoveTail(part2Knots[j-1], part2Knots[j]);
+                        part2Knots[j] = MoveNextKnot(part2Knots[j-1], part2Knots[j]);
                     }
                     part2PositionsVisited.Add(part2Knots.Last());
                 }
             }
 
-            Console.WriteLine($"In part 1 the tail has visited {part1PositionsVisited.Count} positions");
-            Console.WriteLine($"In part 2 the tail has visited {part2PositionsVisited.Count} positions");
+            Console.WriteLine($"In part 1 the tail visited {part1PositionsVisited.Count} positions");
+            Console.WriteLine($"In part 2 the tail visited {part2PositionsVisited.Count} positions");
         }
 
-        static (int,int) MoveTail((int row, int col) newHead, (int row, int col) oldTail) 
+        static (int,int) MoveNextKnot((int row, int col) leader, (int row, int col) follower) 
         {
-            (int row, int col) returnValue = oldTail;
-
-            if (Math.Abs(newHead.row - oldTail.row) <= 1 && Math.Abs(newHead.col - oldTail.col) <= 1)
+            (int row, int col) returnValue;
+            if (Math.Abs(leader.row - follower.row) <= 1 && Math.Abs(leader.col - follower.col) <= 1)
             {
-                returnValue = oldTail;
+                returnValue = follower;
             }
-            else if (newHead.row == oldTail.row)
+            else if (leader.row == follower.row)
             {
-                returnValue = (oldTail.row, oldTail.col + Math.Sign(newHead.col - oldTail.col));
+                returnValue = (follower.row, follower.col + Math.Sign(leader.col - follower.col));
             }
-            else if (newHead.col == oldTail.col)
+            else if (leader.col == follower.col)
             {
-                returnValue = (oldTail.row + Math.Sign(newHead.row - oldTail.row), oldTail.col);
+                returnValue = (follower.row + Math.Sign(leader.row - follower.row), follower.col);
             }
             else
             {
-                returnValue = (oldTail.row + Math.Sign(newHead.row - oldTail.row), oldTail.col + Math.Sign(newHead.col - oldTail.col));
+                returnValue = (follower.row + Math.Sign(leader.row - follower.row), follower.col + Math.Sign(leader.col - follower.col));
             }
 
             return returnValue;
